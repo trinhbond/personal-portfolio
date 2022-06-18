92b44bbfaf741381c6db34d1d02b5ec0f695bd24
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2022-06-18 16:39:03 +1200

    Added Routing for all pages, styled Nav bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AboutMe from "./pages/AboutMe";
+import Home from "./pages/Home";
+import NoSuchPage from './pages/404';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="App-nav">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/aboutme">About Me</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="*" element={<NoSuchPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function NoSuchPage() {
+  return (
+    <div>This page does not exist</div>
+  )
+}
+
