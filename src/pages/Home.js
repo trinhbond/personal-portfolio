5414ef786df30ac86e1c2a12e157514b508e851e
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2022-06-22 19:56:54 +1200

    Created new folders to separate css files and components, additional styling to pages.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
- function Home() {
-    document.title = "Home";
-    return (
-    <div>
-      This is the home page.
-    </div>
+function Home() {
+  document.title = "Home";
+  return (
+  <div>
+    This is the home page.
+  </div>
   )
 }
 
