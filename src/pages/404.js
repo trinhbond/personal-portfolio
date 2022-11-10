139e00ee7ee72bb7c0264170f7851345c9d8f433
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2022-11-10 15:31:23 +1300

    Applied more CSS to the 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,33 @@
-import "../styles/404.css";
-import { Container, Typography, Box } from "@mui/material";
+import '../styles/404.css';
+import { Container, Typography, Box, Button, Link } from '@mui/material';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 
 function PageNotFound() {
-    
     return (
-    <Container className="PageNotFound">
-      <Box>
+    <Container className="container">
+      <Box className="main">
         <Typography variant="h1" component="h1">
           404
         </Typography>
         <Typography variant="body2">
-          Page Not Found!
+          Page Not Found
         </Typography>
+        <Button className="button">
+          <Link className="link" href="/">Go Home</Link>
+        </Button>
+        <Box className="social_icons">
+          <Link href="https://github.com/bondtrinh1" target="blank" >
+            <GitHubIcon fontSize="large" sx={{ color: "#000", mr: 2 }} />
+          </Link>
+          <Link href="https://www.linkedin.com/in/bond-nick-trinh-a015b420a/" target="blank">
+            <LinkedInIcon fontSize="large" sx={{color: "#000", mr: 2 }} />
+          </Link>
+          <Link href="mailto:bond.nicktrinh@gmail.com">
+            <EmailIcon fontSize="large" sx={{ color: "#000" }}/>
+          </Link>
+        </Box>
       </Box>
     </Container>
     )
