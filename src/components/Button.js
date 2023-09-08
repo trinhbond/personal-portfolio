ccbbeeb6f37c7b2cad45968ab4bf26677c716d9f
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2023-09-08 21:23:50 +1200

    Code cleaning css and Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,6 @@
-import styled from "styled-components";
-import classNames from "classnames";
-
-const ButtonStyle = styled.button`
-  background-color: #1ba9d6;
-  border: 1px solid #1ba9d6;
-  border-radius: 32px;
-  color: $white;
-  font-size: 1rem;
-  font-weight: 600;
-  padding: 8px 24px;
-  white-space: nowrap;
-`;
-
 export const Button = ({ className, onClick, children }) => {
   return (
-    <button
-      className={classNames("button", { className })}
-      style={ButtonStyle}
-      onClick={onClick}
-    >
+    <button className={className} onClick={onClick}>
       {children}
     </button>
   );
