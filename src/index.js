2f26eff402126500713e03e7634f29a4df636930
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2023-12-02 16:03:21 +1300

    Add Inter as primary font, create themeprovider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/_app.scss";
+import "./styles/index.scss";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const THEME = createTheme({
+  typography: {
+    fontFamily: "Inter",
+    fontWeightBold: 800,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={THEME}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
