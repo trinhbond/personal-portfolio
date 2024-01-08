f13c37f740ed91cf3bc72504cbab146f7d73a4e2
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-01-08 15:38:29 +1300

    Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import Footer from "./components/Footer";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import Projects from "./pages/Projects";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="h-full flex flex-col justify-between align-center text-white">
+  const Layout = () => (
+    <>
       <Header />
-      <main></main>
-      <Footer />
-    </div>
+      <Outlet />
+    </>
+  );
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="*" element={<>Error Page</>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
