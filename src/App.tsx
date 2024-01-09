c96d68b3a973f5108507719e5fe1df33b40a80e5
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-01-09 21:31:24 +1300

    Content for error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -11,13 +11,25 @@ function App() {
     </>
   );
 
+  const ErrorPage = () => (
+    <div className="max-w-3xl max-[600px]:py-5 py-10 px-5 text-white overflow-x-hidden m-auto">
+      <div className="flex flex-col justify-center align-center gap-3">
+        <h1 className="text-2xl font-medium">Error 404</h1>
+        <p className="text-lg">Oops... This page could not be found. </p>
+        <Link to="/" className="text-lg hover:text-green-300 mt-4">
+          Back to <span className="underline">Home</span>
+        </Link>
+      </div>
+    </div>
+  );
+
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="*" element={<>Error Page</>} />
+          <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
