4ebf5daab71a970baa2c82fa18fc79428b437123
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-01-10 16:01:36 +1300

    Add todo comment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,6 @@
 import { NavLink } from "react-router-dom";
 
-const Header = () => (
-  <header className="max-w-3xl py-8 px-4 max-[600px]:px-6 m-auto text-white">
-    <nav className="flex flex-row justify-between items-center align-center">
-      <NavLink
-        to="/"
-        className="select-none font-semibold text-lg"
-        // className="border-green-300 text-xl select-none border-2 rounded px-2 py-2 leading-5 text-center"
-      >
-        {"<bond />"}
-      </NavLink>
-      <div className="flex flex-row gap-6">
-        <NavLink
-          to="/experience"
-          className={({ isActive }) =>
-            `select-none hover:text-green-300 ${
-              isActive && "text-green-300"
-            } max-[600px]:text-sm`
-          }
-        >
-          Experience
-        </NavLink>
-        <NavLink
-          to="/projects"
-          className={({ isActive }) =>
-            `select-none hover:text-green-300 ${
-              isActive && "text-green-300"
-            } max-[600px]:text-sm`
-          }
-        >
-          Projects
-        </NavLink>
-      </div>
-    </nav>
-  </header>
-);
+/** TODO: Create responsive menu */
+
 
 export default Header;
