95eb2309b871d55e1d73ae8914fb72aa57117be1
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-01-13 14:21:06 +1300

    Remove dead code, add todo comment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,11 @@
 import { NavLink } from "react-router-dom";
 
+// TODO: Make navigation menu responsive
+
 const Header = () => (
   <header className="max-w-3xl py-8 px-4 max-[600px]:px-6 m-auto text-white">
     <nav className="flex flex-row justify-between items-center align-center">
-      <NavLink
-        to="/"
-        className="select-none font-semibold text-lg"
-        // className="border-green-300 text-xl select-none border-2 rounded px-2 py-2 leading-5 text-center"
-      >
+      <NavLink to="/" className="select-none font-semibold text-lg">
         {"<bond />"}
       </NavLink>
       <div className="flex flex-row gap-6">
