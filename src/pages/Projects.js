3d9ba6990089bfb0f2040a53e04d58d922687782
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-01-16 17:47:14 +1300

    Decrease gaps for tag, move projects data to JSON file

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,28 +1,5 @@
-import React from "react";
-import JokesApp from "../random-jokes.png";
-import Portfolio from "../personal-portfolio.png";
 import Link from "../components/ExternalLink";
-
-const projects = [
-  {
-    name: "Jokes App",
-    year: 2023,
-    no: "01",
-    image: JokesApp,
-    description:
-      "A single page web app for fetching a list of random jokes using React Query, built with custom components supported by TypeScript & Material UI.",
-    tech: ["React", "React Query", "Material UI", "SCSS"],
-  },
-  {
-    name: "Personal Portfolio",
-    year: 2022,
-    no: "02",
-    image: Portfolio,
-    description:
-      "A web app showcasing my projects and sharing my professional experience, built with React and hosted on Vercel.",
-    tech: ["React", "Tailwind CSS", "React Router", "SCSS"],
-  },
-];
+import projects from "../projects.json";
 
 function Projects() {
   return (
@@ -45,7 +22,7 @@ function Projects() {
               </Link>
               <p>{project.description}</p>
             </div>
-            <div className="flex flex-row gap-4 flex-wrap text-sm">
+            <div className="flex flex-row gap-2 flex-wrap text-sm">
               {project.tech.map((i) => (
                 <span className="flex-1 max-w-min whitespace-nowrap rounded-md text-center border-2 border-green-300 px-3.5 py-1.5">
                   {i}
