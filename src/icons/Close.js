fed437d6cb75382892b2b361f64c63ef960cb431
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-01-29 21:56:39 +1300

    Increase width for Close icon

--- src/icons/Close.js
+++ src/icons/Close.js
@@ -3,9 +3,9 @@ export const Close = () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       enable-background="new 0 0 24 24"
-      height="22"
+      height="24"
       viewBox="0 0 24 24"
-      width="22"
+      width="24"
       focusable="false"
       style={{
         pointerEvents: "none",
