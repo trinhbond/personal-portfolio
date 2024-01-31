4c30853ee20ae9d7b8cbcf5bff75bcde3becc659
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-01-31 19:34:50 +1300

    Remove Project number from content

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,7 +5,7 @@ import projects from "../projects.json";
 interface ProjectsProps {
   name: string;
   year: number;
-  number: string;
+  number?: string;
   description: string;
   tech: string[];
 }
@@ -18,7 +18,7 @@ function Projects() {
   return (
     <main className="max-w-3xl py-10 px-5 m-auto text-white max-[600px]:text-base overflow-x-hidden flex flex-col gap-12">
       <div>
-        <h1 className="font-semibold text-[26px]">Projects</h1>
+        <h1 className="font-bold text-[26px]">Projects</h1>
         <p>Some projects that I've worked on.</p>
       </div>
       <div className="flex flex-col gap-16 flex-wrap">
@@ -32,14 +32,11 @@ function Projects() {
 
 export const ProjectCard = (project: ProjectsProps) => (
   <div className="animate flex flex-row max-[768px]:flex-col gap-3">
-    <span className="font-bold text-2xl mr-10 max-[600px]:mr-0 max-[600px]:block">
-      {project.number}
-    </span>
     <div className="flex flex-col justify-between gap-6">
       <div>
         <ExternalLink
           href="https://github.com/trinhbond/random-jokes"
-          className="text-green-300 font-medium hover:underline"
+          className="font-medium hover:underline hover:text-green-300"
         >
           {project.name} ({project.year})
         </ExternalLink>
