19daa4a6d7ff5ecd1d0320104d5c0692265c004f
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-02-05 19:12:23 +1300

    Add more content, apply theme color to content

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { ExternalLink } from "../components/ExternalLink";
 
 const Experience = () => {
   useEffect(() => {
@@ -6,18 +7,32 @@ const Experience = () => {
   }, []);
 
   return (
-    <main className="max-w-3xl py-10 px-5 m-auto text-white overflow-x-hidden text-base">
-      <h1 className="font-bold text-[26px] mb-10">Experience</h1>
+    <main className="max-w-3xl py-10 px-5 m-auto text-white overflow-x-hidden flex flex-col gap-12">
+      <div>
+        <h1 className="font-bold text-[26px]">Experience</h1>
+        <p className="text-[#ababab]">
+          Looking to hire?{" "}
+          <ExternalLink
+            href="mailto:bond.nicktrinh@gmail.com"
+            className="underline hover:no-underline hover:text-[#ababab]"
+          >
+            My inbox is always open!
+          </ExternalLink>
+        </p>
+      </div>
       <div className="animate work flex flex-row max-[600px]:flex-col flex-wrap gap-4">
         <div className="font-medium w-1/6 max-[600px]:w-auto whitespace-nowrap">
           2022 - 2023
         </div>
         <div className="w-4/5 max-[600px]:w-auto flex flex-col gap-6">
           <div>
-            <h2 className="font-bold">Junior Software Engineer</h2>
-            <span className="text-[#ababab]">Nui Markets</span>
+            <h2 className="font-semibold inline-block max-[600px]:block">
+              Junior Software Engineer
+            </h2>{" "}
+            <span className="max-[600px]:hidden">{" · "}</span>
+            <span className="font-semibold text-[#ababab]">Nui Markets</span>
           </div>
-          <p>
+          <p className="text-[#ababab]">
             Assumed multiple responsibilities involving frontend development and
             test automation. Typical responsibilities included styling React
             components and collaborating with a UX/UI Designer to bring design
