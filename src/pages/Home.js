da4a397a7e5a701c06d92964d479d7cc3460a7fe
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-05-19 17:07:48 +1200

    New layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,103 +1,144 @@
-import { useEffect } from "react";
-import { ExternalLink } from "../components/ExternalLink";
-import { GitHub } from "../icons/GitHub";
-import { LinkedIn } from "../icons/LinkedIn";
-import { ReactSVG as React } from "../icons/React";
-import { Tailwind } from "../icons/Tailwind";
-import { TypeScript } from "../icons/TypeScript";
-import { Wave } from "../icons/Wave";
-import Avatar from "../avatar.jpg";
+import avatar from "../avatar.jpg";
 import { motion } from "framer-motion";
+import projects from "../projects.json";
+import { Launch } from "../icons/Launch";
 
-const Home = () => {
-  const container = {
-    hidden: { opacity: 1, scale: 0 },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        delayChildren: 0.3,
-        staggerChildren: 0.2,
-      },
-    },
-  };
+interface ProjectProps {
+  name: string;
+  year: number;
+  description: string;
+  tech: string[];
+  link: string;
+}
 
-  const item = {
-    hidden: { y: 20, opacity: 0 },
-    visible: {
-      y: 0,
-      opacity: 1,
-    },
-  };
+const Project = (project: ProjectProps) => (
+  <div className="flex flex-row justify-between gap-8 flex-wrap">
+    <div>
+      <span className="text-[#ababab]">{project.year}</span>
+    </div>
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href={project.link}
+      className="block hover:underline"
+    >
+      {project.name} <Launch />
+    </a>
+    <p className="text-[#e6e6e6]">{project.description}</p>
+    <div className="flex flex-row gap-2 flex-wrap">
+      {project.tech.map((i) => (
+        <span className="flex-1 max-w-min whitespace-nowrap rounded-full text-center text-xs text-[#cdcdcd] bg-[#333333] px-[16px] py-[4px]">
+          {i}
+        </span>
+      ))}
+    </div>
+  </div>
+);
 
-  useEffect(() => {
-    document.title = "Home | Bond Trinh";
-  }, []);
-
+function Home() {
   return (
     <motion.main
-      variants={container}
-      initial="hidden"
-      animate="visible"
-      className="max-w-3xl py-10 px-5 m-auto text-white overflow-x-hidden"
+      initial={{ opacity: 0, scale: 0.5, y: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+      viewport={{ once: true }}
+      whileInView="visible"
+      className="relative text-sm"
     >
-      <motion.div
-        variants={item}
-        className="flex flex-col justify-center align-center gap-16"
-      >
-        <div>
-          <p className="mb-6">
-            Hey, my name is <br />
-            <span className="text-4xl font-bold">Bond Trinh.</span>
-          </p>
-          <p>
-            I am an aspiring software engineer who enjoys front-end development
-            and all things UX/UI. I graduated from AUT with a Bachelor of
-            Computer and Information Sciences, majoring in Software Development.
-            I am currently focused on learning front-end development using
-            modern technologies.
-          </p>
+      <motion.div className="w-full py-6 px-3 flex flex-col justify-center items-center gap-4">
+        <div className="profile w-full py-6 px-3 flex justify-center flex-col gap-6 font-medium">
+          <div className="flex flex-row flex-wrap gap-6 items-center">
+            <div className="w-[80px]">
+              <img className="rounded-full" src={avatar} alt="Dumbass" />
+            </div>
+            <div className="right">
+              <h1>Bond Trinh</h1>
+              <p className="text-[#cdcdcd]">Software Developer</p>
+            </div>
+            <p className="text-[#e6e6e6]">
+              Hey, I'm Bond. I am a software developer with a year of
+              professional development experience. I am passionate about areas
+              in digital design such as UX/UI and frontend development.
+            </p>
+          </div>
         </div>
-        <img
-          src={Avatar}
-          className="max-[600px]:w-1/2 w-1/3 m-auto rounded-full"
-          alt="Idiot"
-        />
-        <div className="flex flex-col gap-2 self-center">
-          <ExternalLink
-            icon={<Wave />}
-            href="mailto:bond.nicktrinh@gmail.com"
-            className="hover:underline hover:text-[#ababab]"
-          >
-            Email me
-          </ExternalLink>
-          <ExternalLink
-            icon={<GitHub />}
-            href="https://www.github.com/trinhbond"
-            className="hover:underline hover:text-[#ababab]"
-          >
-            Check out my repositories
-          </ExternalLink>
-          <ExternalLink
-            icon={<LinkedIn />}
-            href="https://www.linkedin.com/in/bondnicktrinh/"
-            className="hover:underline hover:text-[#ababab]"
-          >
-            Connect with me
-          </ExternalLink>
+
+        <div className="work w-full py-6 px-3 flex justify-center flex-col flex-wrap gap-6 font-medium">
+          <h2>Work Experience</h2>
+          <div className="flex flex-row justify-between gap-6 flex-wrap">
+            <div className="date">
+              <span className="text-[#ababab]">2022 - 2023</span>
+            </div>
+            <div>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={"https://www.nuimarkets.com/"}
+                className="block hover:underline"
+              >
+                Software Engineer at Nui Markets <Launch />
+              </a>
+              <span className="text-[#cdcdcd]">Hybrid</span>
+            </div>
+          </div>
         </div>
-        <div className="self-center">
-          <ul className="flex flex-row gap-8 max-[330px]:flex-col">
-            {[<React />, <TypeScript />, <Tailwind />].map((tool, index) => (
-              <li className="m-auto" key={index}>
-                {tool}
-              </li>
-            ))}
-          </ul>
+
+        <div className="projects w-full py-6 px-3 flex justify-center flex-col flex-wrap gap-6 font-medium">
+          <h2>Side Projects</h2>
+          {projects.map((project, index) => (
+            <Project {...project} key={index} />
+          ))}
+        </div>
+
+        <div className="socials w-full py-6 px-3 flex justify-center flex-col flex-wrap gap-6 font-medium">
+          <h2 className="font-semibold">Connect</h2>
+          <div className="flex flex-row max-[480px]:flex-col gap-10 max-[480px]:gap-0">
+            <span className="w-[75px] text-[#ababab]">Email</span>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={"mailto:bond.nicktrinh@gmail.com"}
+              className="text-[#e6e6e6] hover:underline"
+            >
+              bond.nicktrinh@gmail.com <Launch />
+            </a>
+          </div>
+          <div className="flex flex-row max-[480px]:flex-col gap-10 max-[480px]:gap-0">
+            <span className="w-[75px] text-[#ababab]">GitHub</span>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={"https://www.github.com/trinhbond"}
+              className="text-[#e6e6e6] hover:underline"
+            >
+              trinhbond <Launch />
+            </a>
+          </div>
+          <div className="flex flex-row max-[480px]:flex-col gap-10 max-[480px]:gap-0">
+            <span className="w-[75px] text-[#ababab]">LinkedIn</span>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={"https://www.linkedin.com/in/bondnicktrinh/"}
+              className="text-[#e6e6e6] hover:underline"
+            >
+              Bond Trinh <Launch />
+            </a>
+          </div>
         </div>
       </motion.div>
+      <footer className="px-3 pt-24 pb-12 absolute left-0 right-0">
+        <span className="font-medium">&copy; 2024 Bond Trinh</span>
+      </footer>
     </motion.main>
   );
-};
+}
 
 export default Home;
+
+{
+  /* <div className="mt-4 inline-block w-fit text-[12px] bg-[#0c4a22] rounded-full px-6 py-1 flex flex-row items-center gap-2">
+  <Spotify />
+  <span>Post Malone - Chemicals</span>
+</div>; */
+}
