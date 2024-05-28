644f0d1eb845a99c0cace829bf9675e75b2f7bbf
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-05-28 13:56:04 +1200

    Move Error component from App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import { Error } from "./components/Error";
 
 const App = () => {
-  const Layout = () => <Outlet />;
-
-  const ErrorPage = () => (
-    <div className="max-w-3xl py-5 py-10 px-5 text-white overflow-x-hidden m-auto absolute left-0 right-0 top-0 flex justify-center h-dvh">
-      <div className="flex flex-col justify-center align-center gap-6">
-        <div>
-          <h1 className="text-4xl font-bold">Error 404</h1>
-          <p>This page could not be found. </p>
-        </div>
-        <Link to="/">
-          Back to <span className="underline text-green-300">Home</span>
-        </Link>
-      </div>
-    </div>
+  const Layout = () => (
+    <>
+      <Outlet />
+    </>
   );
 
   return (
@@ -23,7 +14,7 @@ const App = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="*" element={<ErrorPage />} />
+          <Route path="*" element={<Error />} />
         </Route>
       </Routes>
     </BrowserRouter>
