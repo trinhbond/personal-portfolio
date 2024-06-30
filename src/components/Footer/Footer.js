daf8f75fa7b5263ea651a4d3ac2836f349aea995
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-06-30 12:44:59 +1200

    Increase padding on Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="px-3 pt-24 pb-12 absolute left-0 right-0">
+  <footer className="px-6 pt-24 pb-12 absolute left-0 right-0">
     <span className="font-medium">&copy; 2024 Bond Trinh</span>
   </footer>
 );
