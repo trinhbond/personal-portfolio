464f4463a5eced1f64b5fbdea614281b59e9de28
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-06-30 12:39:58 +1200

    Refactor Link component

--- src/components/ExternalLink/ExternalLink.tsx
+++ src/components/ExternalLink/ExternalLink.tsx
@@ -1,14 +1,21 @@
-interface LinkProps {
+interface LinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
   children: React.ReactNode;
-  href: string;
   icon?: React.ReactElement;
-  className?: string;
+  fill?: React.CSSProperties;
+  href: string;
 }
 
-const ExternalLink = ({ children, icon, ...props }: LinkProps) => (
-  <a {...props} target="_blank" rel="noreferrer">
+const ExternalLink: React.FC<LinkProps> = ({
+  children,
+  icon,
+  fill = "#ffffff",
+  href,
+}) => (
+  <a target="_blank" rel="noreferrer" href={href}>
     {icon && (
-      <span className="fill-white mr-3 inline-block align-middle">{icon}</span>
+      <span className={`fill-[${fill}] mr-3 inline-block align-middle`}>
+        {icon}
+      </span>
     )}
     {children}
   </a>
