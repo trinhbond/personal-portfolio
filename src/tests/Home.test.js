4e2aed77e7a9ea2f2995bb118c545d850c938590
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-07-17 14:09:18 +1200

    Delete tests

--- src/tests/Home.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "../pages/Home";
-
-test("renders name in Home page", () => {
-  render(<Home />);
-  const text = screen.getByText(/Bond Trinh./i);
-  expect(text).toBeInTheDocument();
-});
