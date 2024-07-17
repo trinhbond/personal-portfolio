2944604aab7717240ca120bbd61f178cb7e3cfd7
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-07-17 18:59:10 +1200

    <>

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,11 +7,11 @@ export default function NotFound(): JSX.Element {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.8 }}
-      className="h-dvh content-center text-sm"
+      className="h-dvh content-center"
     >
-      <div className="flex flex-col justify-center align-center gap-8 w-max m-auto">
+      <div className="flex flex-col justify-center align-center gap-8 w-max m-auto leading-[1.6rem] text-sm">
         <div>
-          <h1 className="text-5xl font-bold mb-2">Error 404</h1>
+          <h1 className="text-5xl font-bold">Error 404</h1>
           <p className="text-gray-light">This page could not be found. </p>
         </div>
         <Link to="/" className="underline">
