2962870395d5132c2d379768458dd07a5027d9a9
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-07-29 14:51:42 +1200

    Edit link component

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,22 +1,17 @@
 type ExternalLinkProps = {
   children: React.ReactNode;
   icon?: React.ReactElement;
-  fill?: React.CSSProperties;
   href: string;
   className: string;
 };
 
 export default function ExternalLink(props: ExternalLinkProps) {
-  const { icon, fill, children, ...rest } = props;
+  const { icon, children, ...rest } = props;
 
   return (
     <a target="_blank" rel="noreferrer" {...rest}>
       {children}{" "}
-      {icon && (
-        <span className={`fill-[${fill}] inline-block align-middle`}>
-          {icon}
-        </span>
-      )}
+      {icon && <span className="inline-block align-top">{icon}</span>}
     </a>
   );
 }
