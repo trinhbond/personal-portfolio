695e2e0ae11a46ac2adf6cb25c8e0994ae78e555
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-08-07 19:15:25 +1200

    Create Section component

---
+++ src/components/Section.tsx
@@ -0,0 +1,23 @@
+import { motion } from "framer-motion";
+
+type SectionProps = {
+  children: React.ReactNode;
+};
+
+export default function Section({ children }: SectionProps) {
+  const visible = { opacity: 1, y: 0, transition: { duration: 1 } };
+
+  const variant = {
+    hidden: { opacity: 0 },
+    visible,
+  };
+
+  return (
+    <motion.section
+      className="w-full py-6 px-3 flex flex-wrap flex-col gap-6"
+      variants={variant}
+    >
+      {children}
+    </motion.section>
+  );
+}
