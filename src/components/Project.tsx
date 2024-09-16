5f45c2156b5812fe34cf9e197a06cc34ae0137b3
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-09-16 18:29:25 +1200

    Apply bg and round border

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -11,22 +11,21 @@ type ProjectProps = {
 export default function Project(project: ProjectProps): JSX.Element {
   return (
     <div
-      className="flex flex-row justify-between gap-3 flex-wrap"
+      className="bg-[#4f6913] flex flex-col space-between place-content-evenly px-3.5 py-4 rounded-md"
       key={project.name}
     >
       <ExternalLink
-        className="hover:underline font-semibold"
+        className="hover:underline font-semibold w-full"
         href={project.link}
       >
         {project.name}
       </ExternalLink>
-      {/* <span>{project.year}</span> */}
-      <p className="text-gray-light pb-2.5">{project.description}</p>
-      <div className="flex flex-row gap-2 flex-wrap">
+      <p className="text-gray-light text-sm h-[160px] max-[768px]:h-auto mt-4">
+        {project.description}
+      </p>
+      <div className="flex flex-row gap-4 flex-wrap max-[768px]:mt-8">
         {project.tech.map((i) => (
-          <span className="min-w-fit rounded-sm text-center text-xs text-gray-dark bg-[#333333] px-4 py-1.5">
-            {i}
-          </span>
+          <span className="min-w-fit text-xs text-gray-dark">{i}</span>
         ))}
       </div>
     </div>
