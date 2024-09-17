9585e47009932b69d89091df6bc7d3af217b728e
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-09-17 14:07:02 +1200

    remove div

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,98 +7,97 @@ import Section from "../components/Section";
 export default function Home(): JSX.Element {
   return (
     <motion.main
+      className="py-8 flex flex-col items-center gap-8"
       initial="hidden"
       animate="visible"
       exit={{ opacity: 0, transition: { duration: 1 } }}
       variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
     >
-      <div className="py-8 flex flex-col items-center gap-8">
-        <Section>
-          <div className="flex flex-row flex-wrap items-center gap-6">
-            <div className="w-20">
-              <img
-                className="rounded-full"
-                src={require("../assets/images/avatar.jpg")}
-                alt="Bond Trinh"
-              />
-            </div>
-            <div>
-              <h1 className="font-bold text-xl">Bond Trinh</h1>
-              <span className="block text-gray-dark text-sm">
-                Software Developer
-              </span>
-            </div>
-            <p className="text-gray-light">
-              Hi, I'm Bond, a software developer with experience in frontend
-              development, automation testing, and digital design. I am
-              passionate about creating digital experiences that are both
-              visually appealing and highly functional.
-            </p>
+      <Section>
+        <div className="flex flex-row flex-wrap items-center gap-6">
+          <div className="w-20">
+            <img
+              className="rounded-full"
+              src={require("../assets/images/avatar.jpg")}
+              alt="Bond Trinh"
+            />
           </div>
-        </Section>
-
-        <Section>
-          <h2 className="font-bold text-xl">Work</h2>
-          <div className="flex flex-row justify-between gap-6 flex-wrap">
-            <div>
-              <ExternalLink
-                className="block hover:underline font-semibold"
-                href={"https://www.nuimarkets.com/"}
-              >
-                Nui Markets
-              </ExternalLink>
-              <span className="text-gray-dark text-sm">Software Engineer</span>
-            </div>
-            <span>2022 - 2023</span>
-          </div>
-        </Section>
-
-        <Section>
-          <h2 className="font-bold text-xl">Projects</h2>
-          <div className="grid grid-cols-2 max-[768px]:grid-cols-1 gap-4">
-            {projects.map((project) => (
-              <Project {...project} />
-            ))}
-          </div>
-        </Section>
-
-        <Section>
-          <h2 className="font-bold text-xl">Connect</h2>
-          <div className="flex flex-row max-[480px]:flex-col gap-10 max-[480px]:gap-0">
-            <span className="w-20 max-[480px]:w-auto text-gray-darker font-semibold">
-              Email
+          <div>
+            <h1 className="font-bold text-xl">Bond Trinh</h1>
+            <span className="block text-gray-dark text-sm">
+              Software Developer
             </span>
-            <ExternalLink
-              className="text-gray-light hover:underline"
-              href={"mailto:bondtrinh.it@gmail.com"}
-            >
-              bondtrinh.it@gmail.com
-            </ExternalLink>
           </div>
-          <div className="flex flex-row max-[480px]:flex-col gap-10 max-[480px]:gap-0">
-            <span className="w-20 max-[480px]:w-auto text-gray-darker font-semibold">
-              LinkedIn
-            </span>
+          <p className="text-gray-light">
+            Hi, I'm Bond, a software developer with experience in frontend
+            development, automation testing, and digital design. I am passionate
+            about creating digital experiences that are both visually appealing
+            and highly functional.
+          </p>
+        </div>
+      </Section>
+
+      <Section>
+        <h2 className="font-bold text-xl">Work</h2>
+        <div className="flex flex-row justify-between gap-6 flex-wrap">
+          <div>
             <ExternalLink
-              className="text-gray-light hover:underline"
-              href={"https://www.linkedin.com/in/bondnicktrinh/"}
+              className="block hover:underline font-semibold"
+              href={"https://www.nuimarkets.com/"}
             >
-              Bond Trinh
+              Nui Markets
             </ExternalLink>
+            <span className="text-gray-dark text-sm">Software Engineer</span>
           </div>
-          <div className="flex flex-row max-[480px]:flex-col gap-10 max-[480px]:gap-0">
-            <span className="w-20 max-[480px]:w-auto text-gray-darker font-semibold">
-              GitHub
-            </span>
-            <ExternalLink
-              className="text-gray-light hover:underline"
-              href={"https://www.github.com/trinhbond"}
-            >
-              trinhbond
-            </ExternalLink>
-          </div>
-        </Section>
-      </div>
+          <span>2022 - 2023</span>
+        </div>
+      </Section>
+
+      <Section>
+        <h2 className="font-bold text-xl">Projects</h2>
+        <div className="grid grid-cols-2 max-[768px]:grid-cols-1 gap-4">
+          {projects.map((project) => (
+            <Project {...project} />
+          ))}
+        </div>
+      </Section>
+
+      <Section>
+        <h2 className="font-bold text-xl">Connect</h2>
+        <div className="flex flex-row max-[480px]:flex-col gap-10 max-[480px]:gap-0">
+          <span className="w-20 max-[480px]:w-auto text-gray-darker font-semibold">
+            Email
+          </span>
+          <ExternalLink
+            className="text-gray-light hover:underline"
+            href={"mailto:bondtrinh.it@gmail.com"}
+          >
+            bondtrinh.it@gmail.com
+          </ExternalLink>
+        </div>
+        <div className="flex flex-row max-[480px]:flex-col gap-10 max-[480px]:gap-0">
+          <span className="w-20 max-[480px]:w-auto text-gray-darker font-semibold">
+            LinkedIn
+          </span>
+          <ExternalLink
+            className="text-gray-light hover:underline"
+            href={"https://www.linkedin.com/in/bondnicktrinh/"}
+          >
+            Bond Trinh
+          </ExternalLink>
+        </div>
+        <div className="flex flex-row max-[480px]:flex-col gap-10 max-[480px]:gap-0">
+          <span className="w-20 max-[480px]:w-auto text-gray-darker font-semibold">
+            GitHub
+          </span>
+          <ExternalLink
+            className="text-gray-light hover:underline"
+            href={"https://www.github.com/trinhbond"}
+          >
+            trinhbond
+          </ExternalLink>
+        </div>
+      </Section>
     </motion.main>
   );
 }
