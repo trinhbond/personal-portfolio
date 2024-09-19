0d6206bd5e210cf3af5fb66be8323b0b3bd43c68
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-09-20 11:12:41 +1200

    Delete footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
-  const Layout = () => (
-    <>
-      <Outlet />
-      {/* Footer goes here */}
-    </>
-  );
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer(): JSX.Element {
-  return (
-    <footer className="text-center px-3 py-12 absolute left-0 right-0">
-      <span className="text-sm font-medium">&copy; 2024 Bond Trinh</span>
-    </footer>
-  );
-}
