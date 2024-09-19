6f214ff21c7d08f34554271bde8207b2a6fd6bdc
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2024-09-20 11:42:03 +1200

    css

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -16,15 +16,18 @@ export default function NotFound(): JSX.Element {
       variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
     >
       <motion.div
-        className="flex flex-col justify-center align-center gap-8 w-max m-auto leading-[1.6rem] max-[768px]:text-sm"
+        className="flex flex-col justify-center align-center gap-12 w-max m-auto leading-[1.6rem] max-[768px]:text-sm"
         variants={variant}
       >
         <div>
           <h1 className="text-3xl font-bold">Error 404</h1>
           <p className="text-gray-light">This page could not be found. </p>
         </div>
-        <Link to="/" className="underline">
-          Back to Home
+        <Link
+          to="/"
+          className="max-w-fit bg-[#4f6913] rounded-full font-medium py-4 px-8 hover:shadow-2xl"
+        >
+          Home
         </Link>
       </motion.div>
     </motion.main>
