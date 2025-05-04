cf20cb152367b50c8ff6e4498b8223a17b6e3930
bondtrinh1 <88116403+bondtrinh1@users.noreply.github.com>
2025-05-04 19:32:11 +1200

    Change type

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 interface ProjectProps {
-  id: string;
+  id: number;
   name: string;
   year: number;
   description: string;
